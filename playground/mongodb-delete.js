1c9fd0ba08d8549bc22e13aa042b134e4f3261d8
Iwan Darmawan <iwansaila@gmail.com>
2018-07-24 09:49:58 +0700

    script untuk delete

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,22 @@
+const {MongoClient,ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('unable to connect to MongoDB server');
+  } 
+  console.log('Connected to MongoDB Server');
+  //deleteMany
+  db.collection('data_user').deleteMany({jenisKelamin:'Perempuan'}).then((result)=>{
+    console.log(result);
+  });
+  //deleteOne
+  db.collection('data_user').deleteOne({jenisKelamin:'Pria'}).then((result)=>{
+    console.log(result);
+  });
+  //findOneAndDelete
+  db.collection('data_user').findOneAndDelete({jenisKelamin:'Pria'}).then((result)=>{
+    console.log(result);
+  });
+  // db.close();
+});
